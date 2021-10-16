d578d91e6f29898e54223dee494cd56f605a6a97
acvn <aldisatria1996@gmail.com>
2021-10-16 20:03:26 +0700

    asd

---
+++ OOOP/kelasAtauObjek.java
@@ -0,0 +1,10 @@
+class Aldi {
+	int x = 5;
+
+	public static void main(String[] args) {
+		Aldi myObj = new Aldi();
+		Aldi myObj2 = new Aldi();
+		System.out.println(myObj.x);
+		System.out.println(myObj2.x);
+	}
+}
